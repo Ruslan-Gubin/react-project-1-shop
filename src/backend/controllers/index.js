193572f94560bf824c450840cf0e0ddd1d14d795
Ruslan-Gubin <ruslan_gubin@rambler.ru>
2022-10-24 19:23:57 +0300

    отделил backend перед заливкой на versel

--- src/backend/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const apiProductsControllers = require("./api-products-controlers");
-const apiAuthControllers = require("./api-auth-controlers");
-const apiPostControllers = require("./api-post-controlers");
-
-module.exports = {
-  apiProductsControllers,
-  apiAuthControllers,
-  apiPostControllers,
-}
